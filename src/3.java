9dbc9e69af73ac585d5419ff4f632083c0f6302e
ronnygore99 <ronnygore99@gmx.com>
2025-03-06 14:41:51 +0000

    Add 3.java

---
+++ src/3.java
@@ -0,0 +1,9 @@
+import java.util.Random;
+
+public class RandomMath {
+    public static void main(String[] args) {
+        int num1 = (int)(Math.random() * 10);
+        int num2 = (int)(Math.random() * 10);
+        System.out.println("What is " + num1 + " times " + num2);
+    }
+}
